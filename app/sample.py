16812046c71d2397b1a403f5289d3775462328b9
imai_yumelab <93495976+yumelab-imai@users.noreply.github.com>
2023-08-05 15:57:37 +0900

    Update sample.py

--- app/sample.py
+++ app/sample.py
@@ -1,9 +1,1 @@
 print("Hello World!!!")
-
-
-import pandas as pd
-import requests
-from selenium import webdriver
-from time import sleep
-from selenium.webdriver.support.select import Select
-from bs4 import BeautifulSoup
